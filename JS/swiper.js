c9193aea75b19325a7486d3825648669ea79263b
samir-bstb <samir.tiscareno10@gmail.com>
2024-11-30 23:05:23 -0600

    swiper

---
+++ JS/swiper.js
@@ -0,0 +1,25 @@
+new Swiper('.card-wrapper', {
+
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 3,
+ 
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        0:{
+            slidesPerView: 1
+        },
+        768:{
+            slidesPerView: 2
+        },
+        1024:{
+            slidesPerView: 3
+        }
+    }
+
+  });
